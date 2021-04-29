d13425b67849eeccbe74908fb3deb40c8561ce98
Atharva Meshram <atharva10798@gmail.com>
2021-04-29 11:43:07 +0530

    Search and Insert functions added
    Implemented search and insert. Template to be done.

--- bst.cpp
+++ bst.cpp
@@ -59,11 +59,12 @@ class BST{
             struct TreeNode* temp = new struct TreeNode(nums[0]);
             temp->right = nullptr;
             temp->left = nullptr;
-            if (nums[0] < tempRoot->val)
-                tempRoot->left = temp;
-            else
-                tempRoot->right = temp;
-
+            if (tempRoot != nullptr){
+                if (nums[0] < tempRoot->val)
+                    tempRoot->left = temp;
+                else
+                    tempRoot->right = temp;
+            }
             //cout << temp->val << endl;
             return temp;
         }
@@ -76,6 +77,33 @@ class BST{
         node->right = createBST(vector<int>(nums.begin() + mid + 1, nums.end()), node);
 
         return node;
+    }
+
+    struct TreeNode* put(int num, TreeNode* tempRoot){
+        if (tempRoot == nullptr)
+            tempRoot = createBST(vector<int>(1,num));
+        else if (num <= tempRoot->val)
+            tempRoot->left = put(num, tempRoot->left);
+        else if (num > tempRoot->val)
+            tempRoot->right = put(num, tempRoot->right);
+        return tempRoot;
+    }
+
+    void insert(vector<int> nums){
+        for(int i = 0; i < nums.size(); i++)
+            put(nums[i], root);
+    }
+
+    bool Search(int num, TreeNode* tempRoot = nullptr){
+        if (tempRoot == nullptr)
+            tempRoot = root;
+        if (num == tempRoot->val)
+            return true;
+        else if (num < tempRoot->val && tempRoot->left != nullptr)
+            return Search(num, tempRoot->left);
+        else if (num > tempRoot->val && tempRoot->right != nullptr)
+            return Search(num, tempRoot->right);
+        return false;
     }
 
     void display(vector<int> nums){
@@ -106,6 +134,9 @@ int main(){
 
     vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
     BST bst(nums);
+    bst.displayBST();
+    vector<int> a = {8, 0, 10};
+    bst.insert(a);
     bst.displayBST();
     cout << "Height: " << bst.getHeight(bst.root) << endl;
     
